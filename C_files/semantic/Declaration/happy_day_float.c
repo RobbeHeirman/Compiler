5cf398236927f7148db1d806f37c22fcdbd0b9d1
RobbeHeirman <robbe.heirman@student.uantwerpen.be>
2019-07-01 20:43:02 +0200

    More tests written

--- C_files/semantic/Declaration/happy_day_float.c
+++ C_files/semantic/Declaration/happy_day_float.c
@@ -1,5 +1,5 @@
 float a;
 float _foo = 0.456;
-float _4587 = 12;
+float _4587 = 12.0;
 float _assigner = _foo;
-float b, c, d , e = 55,66
+float b, c, d , e = 55.66;
--- test/test_Semantics/declarations_test.py
+++ test/test_Semantics/declarations_test.py
@@ -7,12 +7,22 @@ class STestDeclaration(unittest.TestCase):
     def setUp(self):
         self.path = "C_files/semantic/Declaration/"
 
+    def run_analysis(self, filename):
+        file_name = self.path + filename
+        ast = main.create_ast(file_name)
+        self.assertTrue(ast.semantic_analysis())
+
     def test_integer_happy_day(self):
         """
         Happy day test integer init
-        :return:
         """
+        self.run_analysis("Happy_day_int.c")
 
-        file_name = self.path + "Happy_day_int.c"
-        ast = main.create_ast(file_name)
-        self.assertTrue(ast.semantic_analysis())
+    def test_char_happy_day(self):
+        self.run_analysis("happy_day_char.c")
+
+    def test_float_happy_day(self):
+        self.run_analysis("happy_day_float.c")
+
+    def test_ptr_happy_day(self):
+        self.run_analysis("happy_day_ptr.c")
